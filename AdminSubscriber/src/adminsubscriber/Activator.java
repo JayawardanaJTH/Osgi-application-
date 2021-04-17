3572985f2d1e27319c81a1a12eaaec87af820622
Jayawardana JTH <49198952+JayawardanaJTH@users.noreply.github.com>
2021-04-17 15:05:53 +0530

    Configure activators and classes

--- AdminSubscriber/src/adminsubscriber/Activator.java
+++ AdminSubscriber/src/adminsubscriber/Activator.java
@@ -1,12 +1,22 @@
 package adminsubscriber;
 
+import assignpublisher.IAssign;
+import org.omg.PortableServer.IdAssignmentPolicy;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
 public class Activator implements BundleActivator {
 
+	private ServiceReference<?> assignServiceReference;
+	
+	private IAssign assignService;
 	
 	public void start(BundleContext bundleContext) throws Exception {
+		
+		assignServiceReference = bundleContext.getServiceReference(IAssign.class.getName());
+		
+		assignService = (IAssign) bundleContext.getService(assignServiceReference);
 		
 		MainUI frame = new MainUI();
 		frame.setVisible(true);
@@ -14,6 +24,7 @@ public class Activator implements BundleActivator {
 
 	public void stop(BundleContext bundleContext) throws Exception {
 		
+		bundleContext.ungetService(assignServiceReference);
 	}
 
 }
--- AssignPublisher/src/assignpublisher/Activator.java
+++ AssignPublisher/src/assignpublisher/Activator.java
@@ -2,21 +2,30 @@ package assignpublisher;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
-
-	static BundleContext getContext() {
-		return context;
-	}
+	public ServiceRegistration<?> serviceRegistration;
 
 	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+		
+		AssignImpl assignImpl = new AssignImpl();
+		
+		/*
+		 * Register the publisher
+		 * Here publisher is register using the publisher name and pass the reference for it implementation
+		 */
+		serviceRegistration = bundleContext.registerService(IAssign.class.getName(), assignImpl, null);
+		
+		System.out.println("Assign Service Registerd");
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+		
+		serviceRegistration.unregister();
+		
+		System.out.println("Assign Service Unregisterd");
 	}
 
 }
